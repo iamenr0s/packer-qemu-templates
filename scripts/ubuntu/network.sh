0c884e44f8e474558e69e33d5130cfcbec8bdc70
enros <enros@localhost.localdomain>
2020-08-06 01:03:45 +0100

    modified network script

--- scripts/ubuntu/network.sh
+++ scripts/ubuntu/network.sh
@@ -21,3 +21,23 @@ network:
 EOF
 
 netplan generate
+
+# Ensure a nameserver is being used that won't return an IP for non-existent domain names.
+sed -i -e "s/#DNS=.*/DNS= 208.67.222.222 208.67.220.220/g" /etc/systemd/resolved.conf
+sed -i -e "s/#FallbackDNS=.*/FallbackDNS=/g" /etc/systemd/resolved.conf
+sed -i -e "s/#Domains=.*/Domains=/g" /etc/systemd/resolved.conf
+sed -i -e "s/#DNSSEC=.*/DNSSEC=yes/g" /etc/systemd/resolved.conf
+sed -i -e "s/#Cache=.*/Cache=yes/g" /etc/systemd/resolved.conf
+sed -i -e "s/#DNSStubListener=.*/DNSStubListener=yes/g" /etc/systemd/resolved.conf
+
+# Install ifplugd so we can monitor and auto-configure nics.
+retry apt-get --assume-yes install ifplugd
+
+# Configure ifplugd to monitor the eth0 interface.
+sed -i -e 's/INTERFACES=.*/INTERFACES="eth0"/g' /etc/default/ifplugd
+
+# Ensure the networking interfaces get configured on boot.
+systemctl enable systemd-networkd.service
+
+# Ensure ifplugd also gets started, so the ethernet interface is monitored.
+systemctl enable ifplugd.service
